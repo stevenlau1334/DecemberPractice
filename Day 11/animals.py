2f0e51674a713610016bd138e68a19eb7a4e85c4
Steven Lau <stevenlau1334@gmail.com>
2025-12-19 17:44:27 -0800

    Testing to see if account is linked

--- Day 11/animals.py
+++ Day 11/animals.py
@@ -1,4 +1,7 @@
 class Animal:
     noise = "Rrrr"
     def make_noise(self):
-        print("hello")
+        print(f"{self.noise}")
+        
+    def set_noise(self, new_noise):
+        self.noise = new_noise
