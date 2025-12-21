e7c98db589d29320547558e5c5db9ff353a36f63
Steven Lau <stevenlau1334@gmail.com>
2025-12-20 22:17:56 -0800

    December 20 code

--- Day 11/animals.py
+++ Day 11/animals.py
@@ -1,7 +1,19 @@
 class Animal:
     noise = "Rrrr"
+    color = "Red"
+
     def make_noise(self):
         print(f"{self.noise}")
-        
     def set_noise(self, new_noise):
         self.noise = new_noise
+    def get_color(self):
+        return self.noise
+    def set_solor(self, new_color):
+        self.new_noise = new_color
+        return self.color
+    
+class Wolf(Animal):
+    noise = "grrrr"
+
+class BabyWolf(Wolf):
+    color = "yellow"
